b2b20643e20839733402dcfbc5a7683bc2decd88
Jakub Kuśnierz <jakubkusnierz@gmail.com>
2019-11-09 12:10:40 +0100

    Init app

---
+++ src/main/java/pl/edu/wsiz/waterstation/WaterstationApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wsiz.waterstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterstationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/wsiz/waterstation/WaterstationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wsiz.waterstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
